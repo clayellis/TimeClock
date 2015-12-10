29e707734d8e4ccb5ba4aa98f6eb226e5dd674cf
Edmond Nelson <nelsonje@mtc.byu.edu>
2015-12-09 21:15:35 -0700

    createCard

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
 var TestButton = require('./button');
+var CreateCard = require('./createCard')
 
 var options = {
     title: 'If you see me...'
 };
 
-var element = React.createElement(TestButton, options);
+var element = React.createElement(CreateCard, options);
 React.render(element, document.querySelector('.container'));
