be65f79e14f7778a780af84aa14592842bc9547f
Clay Ellis <clayellis@me.com>
2015-12-10 19:33:59 -0700

    Refactored the whole project to use a new structure. Got ReactRouter working... FINALLY

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,4 @@
-var UsersGrid = require('./users-grid');
-var CreateCard = require('./createCard')
-var Footer = require('./footer')
-var mongoose = require('mongoose')
+var React = require('react');
+var Routes = require('./routes');
 
-var options = {
-    usersInfo: [{
-        username: 'Clay Ellis'
-    },{
-        username: 'Joey Nelson'
-    }],
-};
-
-var element = React.createElement(Footer, options);
-React.render(element, document.querySelector('.container'));
+React.render(Routes, document.querySelector('.container'));
---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default header">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            TimeTracker
+          </Link>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a>Topic #1</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+});
